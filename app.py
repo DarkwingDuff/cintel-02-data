c865f2835fabcec720fce400b4bb34ff114d7d01
DarkwingDuff <dave_duffey@hotmail.com>
2024-10-23 13:49:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import plotly.express as px
 from shiny.express import input, ui
+from shiny.express import output, render, ui
 from shinywidgets import render_plotly
 import palmerpenguins
+from palmerpenguins import load_penguins
 
 penguins_df = palmerpenguins.load_penguins()
+print(penguins_df)
+
+data = penguins_df
+
+ui.input_slider("n", "Number of Bins", 0, 50, 20)
+
+height = 350
+width = "fit-content"
 
 ui.page_opts(title="Dave's Penguin Data", fillable=True)
 with ui.layout_columns():
@@ -15,3 +25,12 @@ with ui.layout_columns():
     @render_plotly
     def plot2():
         return px.histogram(px.data.tips(), y="total_bill")
+
+    @render.data_frame
+    def plot3():
+        return render.DataTable(
+            data, width="auto", height="100%")
+
+    @render.data_frame
+    def plot4():
+        return render.DataGrid(data)
